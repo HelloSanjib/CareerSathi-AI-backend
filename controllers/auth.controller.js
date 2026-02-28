95c31ba674668effb24ed08f09add0f6c41e794f
HelloSanjib <hellosanjib.369@gmail.com>
2026-03-01 03:48:48 +0530

    payment issue fix

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -23,9 +23,9 @@ export const googleAuth = async (req, res) => {
         }
         let token = await genToken(user._id)
         res.cookie("token", token, {
-            http: true,
-            secure: false,
-            sameSite: "strict",
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
@@ -39,7 +39,11 @@ export const googleAuth = async (req, res) => {
 
 export const logOut = async (req, res) => {
     try {
-        await res.clearCookie("token")
+        await res.clearCookie("token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        })
         return res.status(200).json({ message: "LogOut Successfully" })
     } catch (error) {
         return res.status(500).json({ message: `Logout error ${error}` })
